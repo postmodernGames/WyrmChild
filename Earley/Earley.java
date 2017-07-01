81f10000b6e1aaaf5f93fcac5e0bff7473fb6fe7
BrokenGardener <r.graham@mail.utoronto.ca>
2017-07-01 01:34:52 -0400

    Moved state and buildTree to Earley

--- Earley/Earley.java
+++ Earley/Earley.java
@@ -4,10 +4,16 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.ListIterator;
 
+import Node.Node;
 import SymbolTable.SymbolTable;
 import symbol.Symbol;
 import symbol.Symbol.SymbolType;
-import State.State;
+
+//The output of paring should be a queue that represents a parse tree
+//Internally, Earley first builds StateSet
+// Then buildParseTree is run to build the parse tree
+
+
 
 public class Earley {
 	public ArrayList<ArrayList<State>> StateSet;
@@ -36,18 +42,68 @@ public class Earley {
 		reset();
 	}
 
-	public boolean isComplete(State s){
-		//A completed state is one where s = (X-> ...@)
-		return s.rulePosition>=Grammar.get(s.ruleIndex).size()-1;
+
+	public class State{
+		public int ruleIndex;
+		public int rulePosition;
+		public int inputPosition;
+		public String token;
+		public State(int x, int y, int z){
+			ruleIndex = x;
+			rulePosition = y;
+			inputPosition = z;
+		};
+		public State(int x, int y, int z, String s){
+			ruleIndex = x;
+			rulePosition = y;
+			inputPosition = z;
+			token =s;
+		};
+
+
+		public int nextSymbol(ArrayList<ArrayList<Symbol>> Grammar){
+			if(Grammar.get(ruleIndex).size()>rulePosition+1){
+				if(Grammar.get(ruleIndex).get(rulePosition+1).symbolType == SymbolType.nonTerminal)
+					return Grammar.get(ruleIndex).get(rulePosition+1).symbolIndex;
+			}
+			return -1;
+		}
+
+		public boolean isComplete(){
+			//A completed state is one where s = (X-> ...@)
+			return this.rulePosition>=Grammar.get(s.ruleIndex).size()-1;
+		}
+
+
+		public String print(ArrayList<ArrayList<Symbol>> Grammar, ArrayList<Character> dictionary, ArrayList<Character> lexicon){
+			String ret = "";
+			try{
+				for(int i=1; i<Grammar.get(ruleIndex).size(); i++){
+					if(Grammar.get(ruleIndex).get(i).symbolType==SymbolType.terminal){
+						ret+=  lexicon.get(Grammar.get(ruleIndex).get(i).symbolIndex) ;
+					}
+					else ret += dictionary.get(Grammar.get(ruleIndex).get(i).symbolIndex);
+				}
+				ret = ret.substring(0,rulePosition) + '@' + ret.substring(rulePosition,ret.length());
+				ret = dictionary.get(Grammar.get(ruleIndex).get(0).symbolIndex) + "-> " + ret;
+				ret += " : " + ruleIndex + " " + rulePosition + " " + inputPosition;
+				return ret;
+			} catch(java.lang.IndexOutOfBoundsException e) {
+				System.out.println("Error in STATE print");
+			}
+			return ret;
+		}
 	}
-	
-	
-	
-	public boolean processSymbol(Symbol y){
+
+
+
+
+	//Is y always a terminal? Shouldn'y I just pass a char
+	public boolean parseSymbol(Symbol y){
 		boolean accept = false;
 		//for every state in S[inputIndex] 
 		for(State s : StateSet.get(inputIndex)){						
-			if(isComplete(s)) 				//s = (X-> ...@)
+			if(s.isComplete()) 				//s = (X-> ...@)
 				accept = complete(s,inputIndex, Grammar, StateSet, nst.dictionary, tst.dictionary) ||  accept;
 			else{
 				if(Grammar.get(s.ruleIndex).get(s.rulePosition+1).isTerminal()){
@@ -123,4 +179,53 @@ public class Earley {
 		}
 		return completeFlag;
 	}
+
+	public int buildParseTree(ArrayList<Symbol> production, int stateSetIndex, ArrayList<ArrayList<Symbol>> Grammar, Earley Parser){
+		Node current = this;
+		int n = production.size()-1;
+		current.children = new ArrayList<Node>();
+		ensureNodeSize(current.children,n);
+		for(int symbolIndex =n-1; symbolIndex >=0; symbolIndex--){
+			//System.out.println(production.get(ruleIndex));
+			//current.children[ruleIndex] = current;
+			current.children.set(symbolIndex, new Node(production.get(symbolIndex+1)));
+			//current.children[ruleIndex].value = production.get(ruleIndex);
+			if(production.get(symbolIndex+1).category == "nonTerminal"){  //isnonterminal
+				for(State s : Parser.StateSet.get(stateSetIndex)){
+					if(s.rulePosition == Grammar.get(s.ruleIndex).size()-1){  //completed?
+						if(current.children.get(symbolIndex).symbol.number == Grammar.get(s.ruleIndex).get(0).number){  //look for completed rules for this nonterminal within the same S[j]  //EXISTENCE AND UNIQUENESS
+							stateSetIndex = current.children.get(symbolIndex).buildParseTree(Grammar.get(s.ruleIndex), stateSetIndex, Grammar, Parser);
+							break;
+						}
+					}
+				}
+			}
+			else {
+				for(State s : Parser.StateSet.get(stateSetIndex)){
+					if(s.token!=null){
+						current.children.get(symbolIndex).symbol = new Symbol();
+						current.children.get(symbolIndex).symbol.category = "terminal";
+						current.children.get(symbolIndex).symbol.number = Grammar.get(s.ruleIndex).get(s.rulePosition).number;
+						current.children.get(symbolIndex).symbol.token = s.token;
+						//current.children[ruleIndex].value.value = new String(s.token);
+
+						System.out.println(s.token);
+						break;
+					}
+				}
+
+				stateSetIndex--;
+			}
+		}
+		return stateSetIndex;
+	}
+
+	public static void ensureNodeSize(ArrayList<Node> node, int size) {
+		// Prevent excessive copying while we're adding
+		node.ensureCapacity(size);
+		while (node.size() < size) {
+			node.add(new Node());
+		}
+	}
+
 }
--- Node/Node.java
+++ Node/Node.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import DFA.Edge;
 import DFA.Vertex;
 import Earley.*;
-import State.State;
+
 import symbol.Symbol;
 
 public class Node{
@@ -22,52 +22,8 @@ public class Node{
     	symbol.number = s.number;
     	children = new ArrayList<Node>();
     }
-	public int buildParseTree(ArrayList<Symbol> production, int stateSetIndex, ArrayList<ArrayList<Symbol>> Grammar, Earley Parser){
-		Node current = this;
-		int n = production.size()-1;
-		current.children = new ArrayList<Node>();
-		ensureNodeSize(current.children,n);
-		for(int symbolIndex =n-1; symbolIndex >=0; symbolIndex--){
-			//System.out.println(production.get(ruleIndex));
-			//current.children[ruleIndex] = current;
-			current.children.set(symbolIndex, new Node(production.get(symbolIndex+1)));
-			//current.children[ruleIndex].value = production.get(ruleIndex);
-			if(production.get(symbolIndex+1).category == "nonTerminal"){  //isnonterminal
-				for(State s : Parser.StateSet.get(stateSetIndex)){
-					if(s.rulePosition == Grammar.get(s.ruleIndex).size()-1){  //completed?
-						if(current.children.get(symbolIndex).symbol.number == Grammar.get(s.ruleIndex).get(0).number){  //look for completed rules for this nonterminal within the same S[j]  //EXISTENCE AND UNIQUENESS
-							stateSetIndex = current.children.get(symbolIndex).buildParseTree(Grammar.get(s.ruleIndex), stateSetIndex, Grammar, Parser);
-							break;
-						}
-					}
-				}
-			}
-			else {
-				for(State s : Parser.StateSet.get(stateSetIndex)){
-					if(s.token!=null){
-						current.children.get(symbolIndex).symbol = new Symbol();
-						current.children.get(symbolIndex).symbol.category = "terminal";
-						current.children.get(symbolIndex).symbol.number = Grammar.get(s.ruleIndex).get(s.rulePosition).number;
-						current.children.get(symbolIndex).symbol.token = s.token;
-						//current.children[ruleIndex].value.value = new String(s.token);
-						
-						System.out.println(s.token);
-						break;
-					}
-				}
-				
-				stateSetIndex--;
-			}
-		}
-		return stateSetIndex;
-	}
-	public static void ensureNodeSize(ArrayList<Node> node, int size) {
-	    // Prevent excessive copying while we're adding
-	    node.ensureCapacity(size);
-	    while (node.size() < size) {
-	        node.add(new Node());
-	    }
-	}
+
+
 	
 	 void join(Node child, String s, boolean sign){
 	    	Edge e = new Edge(child,s,sign);
