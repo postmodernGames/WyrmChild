1aac0590098b750cfff1da4731d2b06b05258d4c
Ryan Graham <logicalvariable@gmail.com>
2017-07-28 11:45:21 -0400

    Fixed structure

---
+++ Architect/src/main/java/Machine/LayerQueue.java
@@ -0,0 +1,28 @@
+package Machine;
+
+/**
+ * Created by grahamr9 on 7/28/2017.
+ */
+//package LayerQueue;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Created by grahamr9 on 7/25/2017.
+ */
+public class LayerQueue {
+    ArrayList<ArrayList<String>> layers;
+
+    LayerQueue(LinkedList<String> tokenQueue) {
+        layers = new ArrayList<>();
+        ArrayList<String> layer = new ArrayList<>();
+        for (String token : tokenQueue) {
+            if (!token.equals(";")) layer.add(token);
+            else {
+                layers.add((ArrayList<String>) layer.clone());
+                layer.clear();
+            }
+        }
+    }
+}
